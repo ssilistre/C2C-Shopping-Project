2f20b013250bf46512a862c774f0fe8c2f6a20df
SSilistre <24512383+ssilistre@users.noreply.github.com>
2020-11-01 15:36:53 +0300

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('Europe/Istanbul');
 
 try{
-    $db = new PDO('mysql:host=79.98.129.216;dbname=fivem21_market;charset=utf8','fivem21_market','Pr0gramc2');
+    $db = new PDO('mysql:host=*****;dbname=******;charset=utf8','*****','****');
     $db->query("SET CHARACTER SET utf8_general_ci");
    //echo 'Bağlantı Başarıyla gerçekleştirildi !';
 }catch(PDOException $e){
